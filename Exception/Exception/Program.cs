da12a438b1cc423411431041e542ed67c12e46ed
mariama1ieva <maryamfa@code.edu.az>
2023-11-04 20:48:44 +0400

    added tasks

---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,50 @@
+﻿using Exception;
+ 
+
+//Repository<Person> data=new Repository<Person>();
+//data.Add(new Person { Age=22,Name="Meryem",Surname="Aliyeva",Id=1});
+//data.Add(new Person { Age = 23, Name = "Haci", Surname = "Ahmedov", Id = 5 });
+//data.Add(new Person { Age = 28, Name = "Tofiq", Surname = "Nasibli", Id = 11 });
+
+
+//TASK1
+//var datas = data.GetById(11);
+//foreach (Person person in datas)
+//{
+//    Console.WriteLine($"{person.Name},{person.Surname},{person.Age}");
+//}
+
+
+//TASK2
+//var datas = data.Delete(11);
+//foreach(Person person in datas)
+//{
+//    Console.WriteLine($"{person.Name},{person.Surname},{person.Age}");
+//}
+
+
+
+//TASK3
+try
+{
+    int a =int.Parse(Console.ReadLine());
+    
+    if (a>0)
+    {
+
+        int factorial = 1;
+        for (int i = 1; i <= a; i++)
+        {
+            factorial *= i;
+        }
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
+
---
+++ Exception/Exception/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class Repository<T> where T : BaseEntity
+    {
+        private  T[] _datas;
+        public Repository()
+        {
+
+            _datas = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref _datas, _datas.Length+1);
+            _datas[_datas.Length - 1] = item;
+        }
+
+
+        public T[] GetById(int id)
+        {
+
+            return _datas.Where(x => x.Id == id).ToArray();
+        }
+
+        public T[] Delete(int id)
+        {
+            return _datas.Where(x => x.Id != id).ToArray();
+            
+        }
+
+       
+    }
+}
